c2904714f46c404c3ad85da948dfa35253983fb4
Mohamed AIT LAHCEN <mohamedaitlahcen62@gmail.com>
2026-05-22 12:12:24 +0100

    updated footer links

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,14 +2,14 @@ export const site = {
   name: 'Prof OUDAHA ISMAIL',
   nameBold: 'Prof',
   nameLight: 'OUDAHA ISMAIL',
-  channelUrl: 'https://www.youtube.com/@Math.Oudaha_ismail',
+  channelUrl: 'https://youtube.com/@math.oudaha_ismail?si=Az9Bv7uT9iFC4236',
   email: 'Math.ismail98@gmail.com',
   phone: '+212 711‑878800',
   description: 'Cours vidéos et fiches PDF pour les niveaux 1APIC, 2APIC et 3APIC.',
   social: {
-    facebook: 'https://www.facebook.com/profOudaha',
-    instagram: 'https://www.instagram.com/profOudaha',
-    youtube: 'https://www.youtube.com/@Math.Oudaha_ismail',
+    facebook: 'https://www.facebook.com/share/1GSooNZEf9/?mibextid=wwXIfr',
+    instagram: 'https://www.instagram.com/ismail_odh?igsh=YzNmd2drNm1yZ3Zx&utm_source=qr',
+    youtube: 'https://youtube.com/@math.oudaha_ismail?si=Az9Bv7uT9iFC4236',
   },
   footerLinks: [
     { label: 'À Propos', href: '/a-propos/' },
